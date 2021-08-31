d9836300a9db77bf05929ac065eea27c1b5d9c1b
T&R <70495659+Mecknavorz@users.noreply.github.com>
2021-08-31 14:45:14 -0500

    Update Number to Pattern.py
    Simplified some stuff/removed useless things

--- Number to Pattern.py
+++ Number to Pattern.py
@@ -1,12 +1,11 @@
 #recursively convert a number in bae 10 to a pattern of bases (inverse of last problemn)
-def numberToPattern(index, k):
+def numberToPattern(index):
     if index <= 3: #if the only digit remaining is one we can convert to a base just do it
         return toSym(index)
     r = index % 4 #ge the remained of deviding by 4
     letter = toSym(r) #get the letter for the remainder
     preindex = int((index-r)/4) #so we have a whole number when we divide
-    tbr = numberToPattern(preindex, len(str(preindex))) #figure out how to get # of digits
-    return tbr + letter #return the string recursively
+    return numberToPattern(preindex) + letter #return the string recursively
 
 #simple function to convert base to it' # counterpait for frequency array
 def toSym(num):
@@ -21,5 +20,4 @@ def toSym(num):
 
 #find of the data we need to run the equation
 index = input("Number: ")
-k = len(index)
-print(numberToPattern(int(index), k))
+print(numberToPattern(int(index)))
